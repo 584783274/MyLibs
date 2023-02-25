3b035317683a3ed218f084bd9cdd457cb7aa2f32
Your Name <you@example.com>
2023-02-25 11:04:42 +0800

    -

---
+++ src/WeChat/Library/Prpcrypt.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace Kang\Libs\WeChat\Library;
+/**
+ * 微信消息加解密
+ * Class Prpcrypt
+ * @package Kang\Libs\WeChat\Library
+ */
+class Prpcrypt {
+    public $_key;
+    public function __construct($encodingAesKey){
+        $this->_key = base64_decode($encodingAesKey . "=");
+    }
+
+    /**
+     * 对明文进行加密
+     * @param string $text 需要加密的明文
+     * @return string 加密后的密文
+     */
+    public function encrypt($text, $appid) {
+        try {
+            //获得16位随机字符串，填充到明文之前
+            $random = $this->getRandomStr(); //"aaaabbbbccccdddd";
+            $text = $random . pack("N", strlen($text)) . $text . $appid;
+            // 网络字节序
+            $size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+            $iv = substr($this->_key, 0, 16);
+            //使用自定义的填充方式对明文进行补位填充
+            $pkc_encoder = new PKCS7Encoder;
+            $text = $pkc_encoder->encode($text);
+            mcrypt_generic_init($module, $this->_key, $iv);
+            //加密
+            $encrypted = mcrypt_generic($module, $text);
+            mcrypt_generic_deinit($module);
+            mcrypt_module_close($module);
+
+            //			print(base64_encode($encrypted));
+            //使用BASE64对加密后的字符串进行编码
+            return array(ErrorCode::$OK, base64_encode($encrypted));
+        } catch (Exception $e) {
+            //print $e;
+            return array(ErrorCode::$EncryptAESError, null);
+        }
+    }
+
+    /**
+     * 对密文进行解密
+     * @param string $encrypted 需要解密的密文
+     * @return string 解密得到的明文
+     */
+    public function decrypt($encrypted, $appid) {
+        try {
+            //使用BASE64对需要解密的字符串进行解码
+            $ciphertext_dec = base64_decode($encrypted);
+            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+            $iv = substr($this->_key, 0, 16);
+            mcrypt_generic_init($module, $this->_key, $iv);
+            //解密
+            $decrypted = mdecrypt_generic($module, $ciphertext_dec);
+            mcrypt_generic_deinit($module);
+            mcrypt_module_close($module);
+        } catch (\Exception $e) {
+            return [ErrorCode::$DecryptAESError, null];
+        }
+
+        try {
+            //去除补位字符
+            $pkc_encoder = new PKCS7Encoder;
+            $result = $pkc_encoder->decode($decrypted);
+            //去除16位随机字符串,网络字节序和AppId
+            if (strlen($result) < 16) return "";
+            $content = substr($result, 16, strlen($result));
+            $len_list = unpack("N", substr($content, 0, 4));
+            $xml_len = $len_list[1];
+            $xml_content = substr($content, 4, $xml_len);
+            $from_appid = substr($content, $xml_len + 4);
+            if (!$appid)
+                $appid = $from_appid;
+            //如果传入的appid是空的，则认为是订阅号，使用数据中提取出来的appid
+        } catch (\Exception $e) {
+            //print $e;
+            return [ErrorCode::$IllegalBuffer, null];
+        }
+
+        //避免传入appid是错误的情况
+        if ($from_appid != $appid)
+            return [ErrorCode::$ValidateAppidError, null];
+
+        //增加appid，为了解决后面加密回复消息的时候没有appid的订阅号会无法回复
+        return [0, $xml_content, $from_appid];
+    }
+
+    /**
+     * 随机生成16位字符串
+     * @return string 生成的字符串
+     */
+    function getRandomStr() {
+        $str = "";
+        $str_pol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        $max = strlen($str_pol) - 1;
+        for ($i = 0; $i < 16; $i++) {
+            $str .= $str_pol[mt_rand(0, $max)];
+        }
+        return $str;
+    }
+
+}
+
+/**
+ * PKCS7Encoder class
+ *
+ * 提供基于PKCS7算法的加解密接口.
+ */
+class PKCS7Encoder{
+    /**
+     * 对需要加密的明文进行填充补位
+     * @param $text 需要进行填充补位操作的明文
+     * @return 补齐明文字符串
+     */
+    function encode($text) {
+        $text_length = strlen($text);
+        //计算需要填充的位数
+        $amount_to_pad = PKCS7Encoder::$block_size - ($text_length % PKCS7Encoder::$block_size);
+        if ($amount_to_pad == 0) {
+            $amount_to_pad = PKCS7Encoder::$block_size;
+        }
+
+        //获得补位所用的字符
+        $pad_chr = chr($amount_to_pad);
+        $tmp = "";
+        for ($index = 0; $index < $amount_to_pad; $index++) {
+            $tmp .= $pad_chr;
+        }
+
+        return $text . $tmp;
+    }
+
+    /**
+     * 对解密后的明文进行补位删除
+     * @param decrypted 解密后的明文
+     * @return 删除填充补位后的明文
+     */
+    function decode($text) {
+        $pad = ord(substr($text, -1));
+        if ($pad < 1 || $pad > PKCS7Encoder::$block_size) {
+            $pad = 0;
+        }
+
+        return substr($text, 0, (strlen($text) - $pad));
+    }
+
+    public static $block_size = 32;
+}
+
+/**
+ * error code
+ * 仅用作类内部使用，不用于官方API接口的errCode码
+ */
+class ErrorCode {
+    public static $OK = 0;
+    public static $ValidateSignatureError = 40001;
+    public static $ParseXmlError = 40002;
+    public static $ComputeSignatureError = 40003;
+    public static $IllegalAesKey = 40004;
+    public static $ValidateAppidError = 40005;
+    public static $EncryptAESError = 40006;
+    public static $DecryptAESError = 40007;
+    public static $IllegalBuffer = 40008;
+    public static $EncodeBase64Error = 40009;
+    public static $DecodeBase64Error = 40010;
+    public static $GenReturnXmlError = 40011;
+    public static $errCode = [
+        '0' => '处理成功',
+        '40001' => '校验签名失败',
+        '40002' => '解析xml失败',
+        '40003' => '计算签名失败',
+        '40004' => '不合法的AESKey',
+        '40005' => '校验AppID失败',
+        '40006' => 'AES加密失败',
+        '40007' => 'AES解密失败',
+        '40008' => '公众平台发送的xml不合法',
+        '40009' => 'Base64编码失败',
+        '40010' => 'Base64解码失败',
+        '40011' => '公众帐号生成回包xml失败'
+    ];
+
+    public static function getErrText($err) {
+        return  self::$errCode[$err] ?? false;
+    }
+}
--- src/WeChat/Platform.php
+++ src/WeChat/Platform.php
@@ -1,6 +1,21 @@
 <?php
 
 namespace Kang\Libs\WeChat;
+
+use Kang\Libs\WeChat\Library\Comment;
+use Kang\Libs\WeChat\Library\Draft;
+use Kang\Libs\WeChat\Library\Media;
+use Kang\Libs\WeChat\Library\Menu;
+use Kang\Libs\WeChat\Library\MessageFK;
+use Kang\Libs\WeChat\Library\MessageMass;
+use Kang\Libs\WeChat\Library\MessageServer;
+use Kang\Libs\WeChat\Library\MessageTemplate;
+use Kang\Libs\WeChat\Library\Ocr;
+use Kang\Libs\WeChat\Library\OpenApi;
+use Kang\Libs\WeChat\Library\Qrcode;
+use Kang\Libs\WeChat\Library\Tag;
+use Kang\Libs\WeChat\Library\User;
+
 /**
  * 开放平台
  * $wechat = new Platform(['appid' => '', 'appsecret' => '']);
@@ -17,5 +32,29 @@ namespace Kang\Libs\WeChat;
  * @package Kang\Libs\WeChat
  */
 class Platform extends WeChatTrait{
+    use Comment;
+    use Draft;
+    use Media;
+    use Menu;
+    use MessageFK;
+    use MessageMass;
+    use MessageServer;
+    use MessageTemplate;
+    use Ocr;
+    use OpenApi;
+    use Qrcode;
+    use Tag;
+    use User;
 
+    //授权事件类型
+    const AUTHOR_INFO_TYPE_COMPONENT_VERIFY_TICKET = 'component_verify_ticket';
+    const AUTHOR_INFO_TYPE_AUTHORIZED = 'authorized'; //授权成功通知
+    const AUTHOR_INFO_TYPE_UNAUTHORIZED = 'unauthorized'; //取消授权通知
+    const AUTHOR_INFO_TYPE_UPDATEAUTHORIZED = 'updateauthorized'; //授权更新通知
+
+
+    private $_config = [];
+    private $_receive = [];
+    private $_branchId;
+    private $_weObj = null;
 }
